447ee2e28f16eba8077b4faa54021f7c7833629b
Simranjeet Kaur <61777961+simranjeetkaur-maker@users.noreply.github.com>
2021-10-15 22:57:31 +0930

    jobserve file

---
+++ src/test/java/com/simran/job/jobserve/JobserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simran.job.jobserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
